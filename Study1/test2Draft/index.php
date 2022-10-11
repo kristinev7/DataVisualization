7f98b4a06d915fd66847dd46b9f329dbd2cfaacc
kristinev7 <kveneles@gmail.com>
2022-10-11 12:47:14 -0400

    fixed connection path

--- Study1/test2Draft/index.php
+++ Study1/test2Draft/index.php
@@ -1,5 +1,5 @@
 <?php
-    include '../dbconfig.php';
+    include '../../../CPS4745/dbconfig.php';
     $mycon = mysqli_connect($host, $username, $dbpassword, $dbname);
     if (!$mycon) {
         die('Connection error: ' . mysqli_connect_errno());
