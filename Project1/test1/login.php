b4016f397a07e1157430ef994412b8ced6bcdcb4
kristinev7 <kveneles@gmail.com>
2022-10-24 19:18:45 -0400

    working on loginphp

--- Project1/test1/login.php
+++ Project1/test1/login.php
@@ -1,8 +1,13 @@
 <?php
 include '../../../CPS4745/dbconn.php';
+if(isset($_POST['uid']) && isset($_POST['pw'])) {
+    $mycon = mysqli_connect($host, $username, $dbpassword, $dbname);
+    if (!$mycon) {
+        die('Connection error: ' .mysqli_connect_errno());
+    } else {
 
-if(isset($_POST['submit'])) {
-    
-}
+    }//else
+
+}//ifissetuidandpw
 
 ?>
