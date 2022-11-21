865fdc395e236af35cacb842b936f1ea274e0b65
Kristinev7 <kveneles@gmail.com>
2022-11-20 21:37:43 -0500

    treemap dimensions for every object achieved

--- Study2/Treemap/script.js
+++ Study2/Treemap/script.js
@@ -21,47 +21,42 @@ function loadData(data) {
         dataType: 'json',
         success: function(response)
         {
-            console.log(response);
+            //console.log(response);
             stateData = response;
-            console.log(stateData);
+           // console.log(stateData);
+            //console.log(stateData[0]); //returns first obj
+            //console.log(stateData[0].state); //returns NY
+            // var nested = d3.nest()
+            //     .key(function(d,i){ return d.state; })
+            //     .entries(stateData);
+            // console.log(nested);
 
-            // obtain the state nest
-            var state = {"key": "state", 
-                "children":
-                    d3.nest() //nest returns group by state
-                        .key(function(d) {return d.state})
-                        .entries(stateData)
-                    }
-                console.log(state);
-
-            // obtain ny state nest
-            var nystate    = state.children[0]
-            console.log(nystate)
-
-            //var nystateval = {"key":"value",
-            //     "children":
-            //        d3.nest()
-            //            .key(function(d) {return d.nystateval.value})
-            //    
-            //    }
-            //console.log(nystateval)
-
-            let hierarchy = d3.hierarchy(nystate)
+            var newData = {"state": "root", "children": {}};
+            
+            stateData.forEach(function (d) {
+                if (typeof newData.children[d.state] !== 'undefined') {
+                    newData.children[d.state].children.push(d)
+                } else {
+                    newData.children[d.state] = {"state": d.state, "children": [d]}
+                }
+            })
+            newData.children = Object.keys(newData.children).map(function(key) {
+                    return newData.children[key];
+            })
+            console.log("newData:")
+            console.log(newData);
+            let hierarchy = d3.hierarchy(newData)
                     .sum(function(d){ return d.value})
-                    //.sum(
-                    //    (node) => {
-                    //    return node['children']
-                    //})
-                    // .sort(
-                    //     (node1, node2) => {
-                    //         return node2['value'] - node1['value']
-                    //     })
+                    .sort(
+                        (node1, node2) => {
+                            return node2['value'] - node1['value']
+                        })
                 console.log(hierarchy);
                     
             let createTreemap = d3.treemap() 
                     .size([1000,600])(hierarchy)
             
-          // console.log(createTreemap)
+          console.log(createTreemap)
             let stateTiles = createTreemap.leaves()  
             console.log(stateTiles);        
                 
